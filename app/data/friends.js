452d23b25c122b7a3564f998c488f88d2b3796ae
brothernk <brothernk21@hotmail.com>
2018-03-02 22:57:37 -0600

    api routing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,11 @@
-//Empty Array
-var friendArray = [];
+//Friend Array
+var friends = [
+	{
+	 "name": "Meryl Streep",
+	 "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjfLnuW8-zNkP2-8utzWsqvaSDk1vBL--kRVAmZRt2HAYDBh1u_w",
+	 "score": 10
+	}
+];
 
 //Export Array
-module.exports = friendArray;
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,36 @@
 //Loads the friend data
-var friends = require("../data/friends");
+var friends = require("../data/friends.js");
 
 //Routing
 module.exports = function(app) {
   
   //API GET
-  app.get("/api/friendinfo", function(request, response) {
+  app.get("/api/friends", function(request, response) {
     response.json(friends);
   });
 
   // API POST 
-  app.post("/api/friendinfo", function(request, response) {
-    console.log("holla");
+  app.post("/api/friends", function(request, response) {
+
+    var newFriend = request.body;
+    var mostCompatible = {
+      name: "",
+      image: "",
+      score: 1000
+    };
+
+    for (var i = 0; i < friends.length; i ++){
+      var difference = (friends[i].score) - (newFriend.score);
+
+      if (difference <= mostCompatible.score){
+        mostCompatible.name = friends[i].name;
+        mostCompatible.image = friends[i].image;
+        mostCompatible.score = difference;
+      }
+    }
+
+    friends.push(newFriend);
+    response.json(mostCompatible);
+
   });
 };
